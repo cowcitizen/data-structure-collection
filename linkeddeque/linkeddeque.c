351ed425c7fb66a9df9565f60d2598c92373493f
Jaebum Kim <kimjb@dgu.ac.kr>
2024-11-11 17:30:27 +0900

    Add linked deque Added implementation for linked deque. Testing program will be added soon.

---
+++ linkeddeque/linkeddeque.c
@@ -0,0 +1,90 @@
+#include "linkeddeque.h"
+
+void init_deque(Deque* q){
+	q->front = NULL;
+	q->rear = NULL;
+}
+
+void add_front(Deque* q, element e){
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	if(tmp == NULL){
+		fprintf(stderr, "Memory allocation failed.\n");
+		exit(1);
+	}
+
+	tmp->data = e;
+	if(is_empty(q)){
+		q->front = tmp;
+		q->rear = tmp;
+	} else {
+		tmp->llink = NULL;
+		tmp->rlink = q->front;
+		q->front->llink = tmp;
+		q->front = tmp;
+	}
+	return;
+}
+
+void add_rear(Deque* q, element e){
+	Node *tmp = (Node*)malloc(sizeof(Node));
+	if(tmp == NULL){
+		fprintf(stderr, "Memory allocation failed.\n");
+		exit(1);
+	}
+
+	tmp->data = e;
+	if(is_empty(q)){
+		q->front = tmp;
+		q->rear = tmp;
+	} else {
+		tmp->llink = q->rear;
+		tmp->rlink = NULL;
+		q->rear->rlink = tmp;
+		q->rear = tmp;
+	}
+	return;
+}
+
+element delete_front(Deque* q){
+	if(is_empty(q)){
+		fprintf(stderr, "delete_front: Deleted from an empty deque.");
+		exit(2);
+	}
+	element tmp = q->front->data;
+	Node* deleted = q->front;
+
+	q->front->rlink->llink = NULL;
+	q->front = q->front->rlink;
+	free(deleted);
+	return tmp;
+}
+
+element delete_rear(Deque* q){
+	if(is_empty(q)){
+		fprintf(stderr, "delete_rear: Deleted from an empty deque.");
+		exit(2);
+	}
+	element tmp = q->rear->data;
+	Node* deleted = q->rear;
+
+	q->rear->llink->rlink = NULL;
+	q->rear = q->rear->llink;
+	free(deleted);
+	return tmp;
+}
+
+element peek_front(Deque* q){
+	if(is_empty(q)){
+		fprintf(stderr, "peek_front: Peeked an empty deque.");
+		exit(3);
+	}
+	return q->front->data;
+}
+
+element peek_rear(Deque* q){
+	if(is_empty(q)){
+		fprintf(stderr, "peek_rear: Peeked an empty deque.");
+		exit(3);
+	}
+	return q->rear->data;
+}
---
+++ linkeddeque/linkeddeque.h
@@ -0,0 +1,38 @@
+/*
+ * Linked deque: Double-ended queue implemented using a doubly linked list.
+ *
+ * Some implementations for doubly linked lists seem to use the head node instead of two pointers;
+ * I decided to use two pointers because it does the job without problem. 
+*/
+
+#ifndef _LINKEDDEQUE_H
+#define _LINKEDDEQUE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int element;
+typedef struct dequenode{
+	element data;
+	struct dequenode* llink;
+	struct dequenode* rlink;
+} Node;
+
+typedef struct deque{
+	Node* front, * rear;
+} Deque;
+
+void init_deque(Deque*);
+
+void add_front(Deque*, element);
+void add_rear(Deque*, element);
+
+element delete_front(Deque*);
+element delete_rear(Deque*);
+
+element peek_front(Deque*);
+element peek_rear(Deque*);
+
+int is_empty(Deque*);
+
+#endif
